d88476f8ab1ad52264aa685ce5df0e6bff003d57
linhs <linhaisheng@tulinggame.com>
2019-04-01 19:25:06 +0800

    完成 ssr 课程章节 4-9

--- client/index.js
+++ client/index.js
@@ -15,11 +15,26 @@ const router = createRouter()
 const store = createStore()
 
 // 可以动态注册模块
-store.registerModule('c', {
-  state: {
-    text: 3,
-  },
-})
+// store.registerModule('c', {
+//   state: {
+//     text: 3,
+//   },
+// })
+
+// 监听 store 数据变化
+// store.watch((state) => state.count + 1, (newCount) => {
+//   console.log('newCount watch: ' + newCount)
+// })
+
+// store.subscribe((mutation, state) => {
+//   console.log(mutation.type)
+//   console.log(mutation.payload)
+// })
+
+// store.subscribeAction((action, state) => {
+//   console.log(action.type)
+//   console.log(action.payload)
+// })
 
 router.beforeEach((to, from, next) => {
   console.log('global beforeEach')
